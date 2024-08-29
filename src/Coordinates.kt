bc5818c72732f2c11a96cb9a69e2eea5eff89a65
Dmitry <babaykin192@gmail.com>
2024-08-29 19:17:53 +0300

    class Coordinates

---
+++ src/Coordinates.kt
@@ -0,0 +1,35 @@
+class Coordinates(row : Int, col : Int) {
+    private var _columnNumber = col
+    var columnNumber : Int
+        get() = _columnNumber
+        set(value) {
+            _columnNumber = value
+        }
+
+    private var _rowNumber = row
+    var rowNumber : Int
+        get() = _rowNumber
+        set(value) {
+            _rowNumber = value
+        }
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as Coordinates
+
+        if (_columnNumber != other._columnNumber) return false
+        if (_rowNumber != other._rowNumber) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        var result = _columnNumber
+        result = 31 * result + _rowNumber
+        return result
+    }
+
+
+}
