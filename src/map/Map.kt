3dfc17adc9b5e0fc5ad605f39ab234a09fe46ccb
fakechitor <ladasedanbaklajan06@gmail.com>
2024-09-17 02:25:45 +0300

    refactor: little refactor on Map class

--- src/map/Map.kt
+++ src/map/Map.kt
@@ -12,26 +12,22 @@ class Map {
     fun getMap(): MutableMap<Coordinates, Any> {
         return map
     }
-    fun setMap(newMap : MutableMap<Coordinates, Any>) {
-        map = newMap
-    }
-
 
     fun createMap() : MutableMap<Coordinates, Any> {
-        for (i in 1..AMOUNT_OF_COLUMNS) {
-            for (j in 1..AMOUNT_OF_ROWS) {
+        for (i in 1..AMOUNT_OF_ROWS) {
+            for (j in 1..AMOUNT_OF_COLUMNS) {
                 map[Coordinates(i, j)] = ""
             }
         }
         return map
     }
 
-    fun createEntityOnRandomCoordinates(entity : Any){
-        var randomRow : Int
-        var randomColumn : Int
-        while(true){
-            randomRow = Random.nextInt(1, AMOUNT_OF_ROWS)
-            randomColumn = Random.nextInt(1, AMOUNT_OF_COLUMNS)
+    fun createEntityOnRandomCoordinates(entity : Any) {
+        var randomRow: Int
+        var randomColumn: Int
+        while(true) {
+            randomRow = Random.nextInt(1, AMOUNT_OF_ROWS + 1)
+            randomColumn = Random.nextInt(1, AMOUNT_OF_COLUMNS + 1)
             if (isCellEmpty(randomRow, randomColumn)) {
                 setEntity(entity, Coordinates(randomRow, randomColumn))
                 break
@@ -42,21 +38,26 @@ class Map {
     fun getCellStatus(coordinates: Coordinates) : Any? {
         val cellData  = map[coordinates]
         when(cellData){
-            is Herbivore -> return Entities.Rabbit.toString()
-            is Predator -> return Entities.Wolf.toString()
-            is Rock -> return Entities.Rock.toString()
-            is Tree -> return Entities.Tree.toString()
-            is Grass -> return Entities.Grass.toString()
+            is Herbivore -> return Entities.Rabbit
+            is Predator -> return Entities.Wolf
+            is Rock -> return Entities.Rock
+            is Tree -> return Entities.Tree
+            is Grass -> return Entities.Grass
             is String -> return ""
         }
         return cellData
     }
 
     private fun isCellEmpty(row : Int,col : Int) : Boolean {
-        return getCellStatus(Coordinates(row,col)) == ""
+        return getCellStatus(Coordinates(row,col)).toString() == ""
     }
 
     fun setEntity(entity: Any, coordinates : Coordinates){
         map[coordinates] = entity
     }
+    fun removeEntity(coordinates : Coordinates){
+        map.remove(coordinates)
+        map[coordinates] = ""
+    }
+
 }
